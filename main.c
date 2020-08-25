980b642b207d3a111d65621285d2d34ce1618b36
dyalnding <dylanding6464@gmail.com>
2020-08-25 20:49:47 +0000

    New

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <readline/readline.h>
 
 int main(void) {
-  printf("Hello World\n");
+  char *name = readline("Enter your name: ");
+  printf("Hello, %s!\n");
   return 0;
 }
